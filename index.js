c7886fe11ce1e3e2a3d5d2fa03a9f5849c2fdc92
Saif-2019331014 <smsaifullah29@gmail.com>
2023-10-07 05:24:59 +0600

    mining reward added

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 import sha256 from "crypto-js/sha256.js";
 
 class Block {
-  constructor(timestamp, data, previousHash = "0000") {
+  constructor(timestamp, transactions, previousHash = "0000") {
     this.timestamp = timestamp;
-    this.data = data;
+    this.transactions = transactions;
     this.previousHash = previousHash;
+    this.nonce = 29;
     this.hash = this.calculateHash();
-    this.nonce = 0;
   }
 
   calculateHash() {
     return sha256(
       this.timestamp +
-        JSON.stringify(this.data) +
+        JSON.stringify(this.transactions) +
         this.previousHash +
         this.nonce
     ).toString();
   }
 
   mineBlock(difficulty) {
-    while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
-        this.nonce++
-        this.hash = this.calculateHash()
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.calculateHash();
     }
 
-    console.log('Mining done. Hash:' + this.hash)
+    // console.log("Mining done. Hash:" + this.hash);
+  }
+}
+
+class Transaction {
+  constructor(fromAddress, toAddress, amount) {
+    this.fromAddress = fromAddress;
+    this.toAddress = toAddress;
+    this.amount = amount;
   }
 }
 
@@ -32,12 +42,14 @@ class Blockchain {
   constructor() {
     this.chain = [this.generateGenesisBlock()];
     this.difficulty = 4;
+    this.pendingTransactions = [];
+    this.miningReward = 10;
   }
 
   generateGenesisBlock() {
     return new Block(
       "2000-02-19",
-      { itemType: "initial block", name: "GENESIS BLOCK", price: "$0" },
+      [new Transaction("amarAddress", "tomarAddress", 2000)],
       "0000"
     );
   }
@@ -46,10 +58,36 @@ class Blockchain {
     return this.chain[this.chain.length - 1];
   }
 
-  appendBlock(newBlock) {
-    newBlock.previousHash = this.getCurrentLastBlock().hash;
-    newBlock.mineBlock(this.difficulty)
-    this.chain.push(newBlock);
+  appendTransaction(transaction) {
+    this.pendingTransactions.push(transaction);
+  }
+
+  minePendingTransactions(minerAddress) {
+    const block = new Block(new Date().toUTCString(), this.pendingTransactions);
+    block.previousHash = this.getCurrentLastBlock().hash;
+    block.mineBlock(this.difficulty);
+    this.chain.push(block);
+    this.pendingTransactions = [
+      new Transaction("System", minerAddress, this.miningReward),
+    ];
+  }
+
+  getAddressBalance(address) {
+    let balance = 0;
+
+    for (const block of this.chain) {
+      for (const transaction of block.transactions) {
+        if (address === transaction.fromAddress) {
+          balance -= transaction.amount;
+        }
+
+        if (address === transaction.toAddress) {
+          balance += transaction.amount;
+        }
+      }
+    }
+
+    return balance;
   }
 
   isBlockchainValid() {
@@ -70,17 +108,17 @@ class Blockchain {
 }
 
 const hopecoin = new Blockchain();
-const block1 = new Block(new Date().toUTCString(), {
-  itemType: "book",
-  name: "Blockchain 101",
-  price: "$10"
-});
-const block2 = new Block(new Date().toUTCString(), {
-    itemType: "book",
-    name: "How to train your dragon",
-    price: "$20"
-  });
+const transaction1 = new Transaction("address1", "address2", 100);
+const transaction2 = new Transaction("address2", "address1", 50);
+hopecoin.appendTransaction(transaction1);
+hopecoin.appendTransaction(transaction2);
+hopecoin.minePendingTransactions("Um_ded");
+// console.log(hopecoin.getAddressBalance('Um_ded')) /// Reward is in pending transactions
+hopecoin.minePendingTransactions("Tum_ded");
+// console.log(hopecoin.getAddressBalance('Um_ded')) /// Reward in account because got mined
 
-hopecoin.appendBlock(block1);
-hopecoin.appendBlock(block2);
+// console.log(hopecoin.getAddressBalance('amarAddress'))
+// console.log(hopecoin.getAddressBalance('tomarAddress'))
+// console.log(hopecoin.getAddressBalance('address1'))
+// console.log(hopecoin.getAddressBalance('address2'))
 console.log(hopecoin);
